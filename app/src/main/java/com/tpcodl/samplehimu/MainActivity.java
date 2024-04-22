ca390ab5d1f463078318b02f295e070eec0f4c17
Rishikesh Rahi <rishikesh.rahi@tpcentralodisha.com>
2024-04-22 11:14:31 +0530

    testweb

--- app/src/main/java/com/tpcodl/samplehimu/MainActivity.java
+++ app/src/main/java/com/tpcodl/samplehimu/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.tpcodl.mylibdemo.MyLibrary;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyLibrary.openWebView(MainActivity.this);
     }
 }
--- mylibdemo/src/main/java/com/tpcodl/mylibdemo/MyLibrary.java
+++ mylibdemo/src/main/java/com/tpcodl/mylibdemo/MyLibrary.java
@@ -1,8 +1,17 @@
 package com.tpcodl.mylibdemo;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
 public class MyLibrary {
 
     public static int add(int a, int b) {
         return a + b;
+    }
+
+    public static void openWebView(Context mContext){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+        mContext.startActivity(browserIntent);
     }
 }
